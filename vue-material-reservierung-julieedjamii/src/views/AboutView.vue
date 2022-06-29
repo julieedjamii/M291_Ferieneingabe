3670e7ac80f6c291b47208009526469dff036182
julieedjamii <juliette.boulatova@sbw-media.ch>
2022-06-29 13:51:06 +0200

    update

--- vue-material-reservierung-julieedjamii/src/views/AboutView.vue
+++ vue-material-reservierung-julieedjamii/src/views/AboutView.vue
@@ -4,13 +4,11 @@
     <img alt="holztisch" src="../assets/holztisch.jpg">
     <img alt="grafiker" src="../assets/grafiker.jpg">
     <img alt="reden" src="../assets/reden.jpg">
-    <img alt="sbwhaus" src="../assets/sbwhaus.jpg">
-    <img alt="schule" src="../assets/schule.jpg">
 
   </div>
   <p>
     Ich wollte als Abschlussprojekt etwas nützliches erstellen, dass die im Betriebsalltag einiges erleichtert. 
     Mit dieser Seite kann man seine Ferien selber eingeben und sie auf einer Datenbank speichern. 
-    So kann man sehen wer von wann bis wann in den Ferien ist. 
+    So kann man sehen wer von wann bis wann in den Ferien ist. ich hoffe es gefällt dir Joki:)
   </p>
 </template>
